816cb9023bac51125d500f9d4a9c31710d91022a
mayara merlin <mayaramerlin@hotmail.com>
2023-04-06 19:18:30 -0300

    módulo 2

--- Back/src/MM.API/Models/Evento.cs
+++ Back/src/MM.API/Models/Evento.cs
@@ -1,18 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MM.API.Models
+﻿namespace MM.WebApi.Models
 {
     public class Evento
     {
-        public int EventoId { get; set; }
+        public int Id { get; set; }
         public string Local { get; set; }
-        public string DataEvento { get; set; }
+        public DateTime? DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
-        public string Lote { get; set; }
         public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
     }
 }
