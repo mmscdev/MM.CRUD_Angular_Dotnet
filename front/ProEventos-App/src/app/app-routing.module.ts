6e029c9ee15fedd2a155ff5cd12c001cdc012aec
mayara merlin <mayaramerlin@hotmail.com>
2023-04-14 19:30:56 -0300

    roteamento

--- front/ProEventos-App/src/app/app-routing.module.ts
+++ front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eventos', component: EventosComponent},
+  {path: 'palestrantes', component: PalestrantesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'contato', component: ContatosComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/ProEventos-App/src/app/shared/nav/nav.component.ts
+++ front/ProEventos-App/src/app/shared/nav/nav.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { BsDropdownConfig } from "ngx-bootstrap/dropdown";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'] ,
+  providers: [
+    {
+      provide: BsDropdownConfig,
+      useValue: { isAnimated: true, autoClose: true }
+    }
+  ]
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
